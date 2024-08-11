0b0481bc11537e3774c58eb480bf6677325934e4
Ridwan Akinola <akinolaridwanbnhusaain@gmail.com>
2024-08-10 23:43:08 -0700

    update

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,9 @@
-/* eslint-disable */
-export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {});
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
